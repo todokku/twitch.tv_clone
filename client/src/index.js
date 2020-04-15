3158a3ffafe195171cfdd2335426dd6947130a20
Christian G <chrisgobin94@gmail.com>
2020-04-15 13:38:07 -0400

    fixed render issue

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from ".components/App";
+import App from "./components/App";
 
-ReactDOM.render(
-  <React>
-    <App />
-  </React>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
