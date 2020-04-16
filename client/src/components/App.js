981ead4f0ec2ac5f9ec5dfddc2e898259cb55fb2
Christian G <chrisgobin94@gmail.com>
2020-04-15 22:33:03 -0400

    Router Basics
    Basic wiring of Router Components.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,43 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return <div className="App">This is the Client.</div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Route path="/" exact component={Home} />
+        <Route path="/availablestreams" component={AvailableStreams} />
+        <Route path="/startstream" component={StartStream} />
+      </div>
+    </Router>
+  );
+};
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to my Twitch Clone</h1>
+    </div>
+  );
+};
+
+const AvailableStreams = () => {
+  return <div>These Are the Available Streams</div>;
+};
+
+const StartStream = () => {
+  return (
+    <div>
+      Start Your Stream. <br />
+      <button
+        onClick={(e) => {
+          console.log("Stream Started");
+        }}
+      >
+        Launch Client
+      </button>
+    </div>
+  );
+};
+
 export default App;
